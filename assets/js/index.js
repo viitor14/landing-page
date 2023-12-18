174f152590c27b0efa0925e29d29e1a66a35a8b1
viitor14 <manoelvitor550@gmail.com>
2023-12-18 17:55:37 -0300

    Landing Page Ballecoffee

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+function clickNav() {
+  if ((abrirNav.style.display = "none")) {
+    abrirNav.style.display = "block";
+  } else {
+    abrirNav.style.display = "none";
+  }
+}
+
+function clickFecharNav() {
+  if ((abrirNav.style.display = "block")) {
+    abrirNav.style.display = "none";
+  }
+}
